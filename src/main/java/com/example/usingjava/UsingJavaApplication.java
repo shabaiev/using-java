d24fb270d130fa38919ab1cd4052e1ea56370fc0
Illia Shabaiev <ilyashabaev2209@gmail.com>
2019-06-12 17:42:52 -0500

    First commit

---
+++ src/main/java/com/example/usingjava/UsingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.usingjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingJavaApplication.class, args);
+	}
+
+}
